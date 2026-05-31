e9ea1bc32dac9e8ed47bad80738e3ea6ef3fe621
kraj2503 <73212876+kraj2503@users.noreply.github.com>
2026-05-31 22:24:53 +0530

    start sping init

---
+++ src/main/java/com/raj/quickstart/helloWorldController.java
@@ -0,0 +1,15 @@
+package com.raj.quickstart;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloWorldController {
+
+
+    @GetMapping(path = "/hello")
+    public String helloWorld(){
+        return "hello World";
+    }
+}
